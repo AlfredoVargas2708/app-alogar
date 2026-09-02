fed15f325fb1990d86edfb760e22bb6595a74ae3
Alfredo Vargas <avargas@finmarkets.cl>
2026-09-02 12:59:32 -0400

    Add apiToken configuration for axios instance; include Authorization header

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
 
+const apiToken =
+  import.meta.env.VITE_API_TOKEN ??
+  'a10ca26e0f94f01e6001c0044f305f5c7387a9adf3cfeedfa15f09aa08ab11ae'
+
 const api = axios.create({
   baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${apiToken}`,
   },
 })
 
